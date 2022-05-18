9811912ddf70481f4b9a521e61964a240b07e8b4
KVenLucas <k_l.s@outlook.com>
2022-05-18 20:49:25 -0300

    feat: implementation on count lenght array

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length_list(list, 0)
+
+  defp length_list([], acc), do: acc
+
+  defp length_list([_ | tail], acc) do
+    acc = acc + 1
+    length_list(tail, acc)
+
   end
 end
